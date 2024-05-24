12c34dd79f85dd5c4c7057dce493da91a28460ee
mrStudDev <marcosscfloripa@gmail.com>
2024-05-24 18:28:43 -0300

    voltando

--- app_manager/views.py
+++ app_manager/views.py
@@ -17,31 +17,24 @@ class ManagerView(ListView):
     model = PageView
     template_name = 'templates_manager/manager_admin.html'
     
-    def get_context_data(self, **kwargs):
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
-
-        # Buscando todos os PageViews
+        
         context['page'] = PageView.objects.all()
-
-        # Ordenando os PageViews pela contagem de visualizações (decrescente)
         context['pages'] = PageView.objects.all().order_by('-view_count')
+        
+        context['total_views'] = PageView.objects.aggregate(total=Sum('view_count'))['total'] or 0
 
-        # Formatando a data e hora em cada PageView
-        for page in context['pages']:
-            page.last_accessed = page.last_accessed.strftime("%d de %b de %Y às %H:%M")
-
-        # Calculando o total de visualizações de todas as páginas
-        total_views_all_pages = PageView.objects.aggregate(total=Sum('view_count'))['total'] or 0
-        context['total_views_all_pages'] = total_views_all_pages  # Novo nome para evitar conflito
-
-        # Configurações adicionais para o template (provavelmente relacionadas ao layout)
         context["hide_sidebar"] = True
         context['current_app'] = 'app_manager'
 
-        # Obtendo as visualizações diárias de cada página
         daily_views = DailyPageView.objects.values('date').annotate(total=Sum('view_count')).order_by('-date')
         context['daily_views'] = daily_views
-
+        
+        total_views = PageView.objects.aggregate(total=Sum('view_count'))['total'] or 0
+        
+        context['total_views'] = total_views
+        
         return context
     
 def update_page_view(page_name):
